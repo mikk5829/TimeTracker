cb9ede42377a13099ace5aa4133d27f9796e08e7
Mikkel <s184230@student.dtu.dk>
2022-03-18 11:25:00 +0100

    eslint

--- src/pages/Home2.tsx
+++ src/pages/Home2.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, Stack, Typography, Divider} from "@mui/material";
+import {Divider, Stack} from "@mui/material";
 import TimerM from "../components/Timer";
 import TimerA from "../components/Timer2";
 import TimerT from "../components/Timer3";
@@ -7,26 +7,26 @@ import TimerT from "../components/Timer3";
 export default function Home2() {
     return (
         <Stack spacing={1} direction='row' alignItems={"center"} alignContent={"center"}
-        divider={<Divider orientation="vertical" flexItem />}>
-            <TimerM />
-            <TimerA />
-            <TimerT />
-            <TimerM />
-            <TimerA />
-            <TimerT />
-            <TimerM />
-            <TimerA />
-            <TimerT />
-            <TimerM />
-            <TimerA />
-            <TimerT />
-            <TimerM />
-            <TimerA />
-            <TimerT />
-            <TimerM />
-            <TimerA />
-            <TimerT />
-        </Stack> )
+               divider={<Divider orientation="vertical" flexItem/>}>
+            <TimerM/>
+            <TimerA/>
+            <TimerT/>
+            <TimerM/>
+            <TimerA/>
+            <TimerT/>
+            <TimerM/>
+            <TimerA/>
+            <TimerT/>
+            <TimerM/>
+            <TimerA/>
+            <TimerT/>
+            <TimerM/>
+            <TimerA/>
+            <TimerT/>
+            <TimerM/>
+            <TimerA/>
+            <TimerT/>
+        </Stack>)
 
         ;
 }
