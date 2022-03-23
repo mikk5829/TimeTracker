dabf209dd0f0da21b5a27a2f469286017abd224c
Mikkel <s184230@student.dtu.dk>
2022-03-23 15:06:12 +0100

    New colors and fonts

---
+++ src/service/theme.ts
@@ -0,0 +1,46 @@
+import {createTheme} from "@mui/material";
+
+export const theme = createTheme({
+    typography: {
+        fontFamily: [
+            'Epilogue',
+            'sans-serif',
+        ].join(','),
+        h1: {
+            fontWeight: 700
+        },
+        h2: {
+            fontWeight: 700
+        },
+        h3: {
+            fontWeight: 700
+        },
+        h4: {
+            fontWeight: 700
+        },
+        h5: {
+            fontWeight: 700
+        },
+        h6: {
+            fontWeight: 700
+        },
+        body1: {
+            fontWeight: 400
+        },
+        body2: {
+            fontWeight: 400
+        }
+    },
+    palette: {
+        primary: {
+            main: '#304255',
+            light: '#5B6D82',
+            dark: '#304255'
+        },
+        secondary: {
+            main: '#CA4A53',
+            light: '#FF7B7F',
+            dark: '#94132B'
+        }
+    }
+});
