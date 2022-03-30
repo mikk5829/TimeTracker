8e98ee690ae0ba7a6979fe231dbd091bad2f592e
alaym3 <aam5xa@virginia.edu>
2022-03-30 14:40:43 +0200

    Add categories

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,25 @@
 import * as React from 'react';
-import {Button, IconButton, Stack, Typography} from "@mui/material";
+import {Button, Dialog, DialogActions, DialogTitle, IconButton, Stack, TextField, Typography} from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
 import EventItem from "../components/EventItem";
+import {usePersistState} from "../service/state";
+import {useState} from "react";
+
+function AddCategoryDialog() {
+
+}
 
 export default function Home() {
+    const [categories, setCategories] = usePersistState([], "categories") // store tracked categories
+
+    const [openAddCategoryDialog, setOpenAddCategoryDialog] = useState(false) // open dialog to add categories
+
+    const [addCategoryText, setAddCategoryText] = useState("") // what the user types to add as a category
+
+    const AddCategory = (categoryName: string) => {
+        setCategories((categories: any) => [...categories, categoryName]) // append the previous categories with new one
+    }
+
     return (
         <div>
             <Typography variant={"h1"}>TimeTracker</Typography>
@@ -12,18 +28,35 @@ export default function Home() {
                     direction="row"
                     justifyContent="space-between"
                     alignItems="center"
-                    spacing={0}
+                spacing={0}
                 >
                     <Typography variant={"h2"}>Add Events</Typography>
-                    <IconButton aria-label="add" disabled color="primary">
+                    <IconButton aria-label="add"  color="primary" onClick={() => setOpenAddCategoryDialog(true)}>
                         <AddIcon/>
                     </IconButton>
                 </Stack>
-                <EventItem eventName="Reading"/>
-                <EventItem eventName="Running"/>
-                <EventItem eventName="Cooking"/>
-                <EventItem eventName="Commute"/>
+                {categories.map((cat: string) => {
+                    return <EventItem eventName={cat}/> // stack all the user's specified categories
+                })}
+
             </Stack>
+            <Dialog open={openAddCategoryDialog}>
+                <DialogTitle>
+                    Add a new category!
+                </DialogTitle>
+                <TextField value={addCategoryText} onChange={event => setAddCategoryText(event.target.value)}>
+
+                </TextField>
+                <DialogActions>
+                    <Button variant="contained" color="secondary" onClick={() => {
+                        AddCategory(addCategoryText) // add whatever the user typed to the category list
+                        setAddCategoryText("") // clear it out afterwards
+                        setOpenAddCategoryDialog(false) // close the dialog
+                    }}>
+                        Save
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </div>
     );
 }
