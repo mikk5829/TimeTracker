300dc961fbc812e2196a33fbf4750b3b84ef8ec8
Mikkel <s184230@student.dtu.dk>
2022-03-30 16:16:35 +0200

    Apex charts

--- src/pages/Trends.tsx
+++ src/pages/Trends.tsx
@@ -1,10 +1,43 @@
 import * as React from 'react';
 import {Typography} from "@mui/material";
+import ReactApexChart from "react-apexcharts";
 
 export default function Trends() {
+    const state = {
+        options: {
+            chart: {
+                id: "basic-bar"
+            },
+            plotOptions: {
+                bar: {
+                    borderRadius: 8
+                }
+            },
+            dataLabels: {
+                enabled: false
+            },
+            xaxis: {
+                categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+            }
+        },
+        series: [
+            {
+                name: "series-1",
+                data: [30, 40, 45, 50, 49, 60, 70, 91]
+            }
+        ]
+    };
+
     return (
         <div>
             <Typography variant={"h1"}>Trends</Typography>
+            <ReactApexChart
+                options={state.options}
+                series={state.series}
+                type="bar"
+                height="auto"
+                width="100%"
+            />
         </div>
     );
 }
