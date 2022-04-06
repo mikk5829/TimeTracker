263ec2a3d8f4a0a5bc10d64d5a0da978c43149d9
alaym3 <aam5xa@virginia.edu>
2022-04-06 14:18:40 +0200

    Add trend options

--- src/pages/Trends.tsx
+++ src/pages/Trends.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Typography} from "@mui/material";
+import {Stack, Typography} from "@mui/material";
 import ReactApexChart from "react-apexcharts";
 
 export default function Trends() {
@@ -17,13 +17,24 @@ export default function Trends() {
                 enabled: false
             },
             xaxis: {
-                categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+                categories: ["January", "February", "March", "April", "May", "June", "July", "August", "September"]
             }
         },
         series: [
             {
-                name: "series-1",
+                name: "Reading",
                 data: [30, 40, 45, 50, 49, 60, 70, 91]
+            },
+            {
+                name: "Studying",
+                type: "column",
+                data: [23, 12, 54, 61, 32, 56, 81, 19]
+            }
+            ,
+            {
+                name: "Exercise",
+                type: "column",
+                data: [4, 32, 12, 41, 18, 81, 73, 43]
             }
         ]
     };
@@ -31,13 +42,32 @@ export default function Trends() {
     return (
         <div>
             <Typography variant={"h1"}>Trends</Typography>
+
+            <Typography align="center" variant={"h3"}>Stacked bar</Typography>
             <ReactApexChart
                 options={state.options}
                 series={state.series}
                 type="bar"
                 height="auto"
                 width="100%"
+                title="hey"
             />
+            <Typography align="center" variant={"h3"}>Line</Typography>
+            <ReactApexChart
+                type="line"
+                options={state.options}
+                series={state.series}
+                height={350}
+                />
+
+            <Typography align="center" variant={"h3"}>Area</Typography>
+            <ReactApexChart
+                type="area"
+                options={state.options}
+                series={state.series}
+                height={350}
+            />
+
         </div>
     );
 }
