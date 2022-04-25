61e3aa943f4375e99c40fb8970c51540df9c2ffb
thw-desktop <thomaswitting97@gmail.com>
2022-04-25 16:21:04 +0200

    Total, monthly, weekly plots added

--- src/pages/Trends.tsx
+++ src/pages/Trends.tsx
@@ -11,27 +11,46 @@ export default function Trends() {
 
     const [{categories, events, error}, dispatch] = usePersistReducer() // useReducer(reducer, initialState);
 
-    let totalSeries: { name: string, type: string, data: number[] }[] = [{name: "abc", type: "column", data: []}];
+    // FOR STORING TOTAL HOURS SPENT
+    let totalSeries: { name: string, data: number[] }[] = [{name: "abc", data: []}];
     let totalXLabels: string[] = [];
 
-    let idxs: {id: string, index: number}[] = [];
+    // FOR STORING MONTHLY TIME SPENT
+    let monthlyXLabels: string[] = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let monthlySeries: { name: string, data: number[] }[] = [];
+
+    // FOR STORING WEEKLY TIME SPENT
+    let weeklyXLabels: string[] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    let weeklySeries: { name: string, data: number[] }[] = [];
+
+    let idxs: { id: string, index: number }[] = [];
     let i = 0;
 
     events.forEach((event: Event) => {
 
         let id = event.categoryId;
 
-        totalXLabels.push(event.categoryId);
+        totalXLabels.push(id);
+        let startTime = moment(event.startTime);
+        let endTime = moment(event.endTime);
 
-        var duration = moment.duration(moment(event.endTime).diff(event.startTime));
+        let weekday = startTime.day();
+        let date = startTime.date(); // 1-based (first day of month = 1)
+        let month = startTime.month(); // 0-based (jan=0, dec=11)
+        let year = startTime.month();
+
+        var duration = moment.duration(endTime.diff(startTime));
         var hours = duration.asHours();
 
 
-        var match_index = idxs.find(x => x.id == id);
+        var match_total = idxs.find(x => x.id == id);
+        var match_month = monthlySeries.find(x => x.name == id);
+        var match_week = weeklySeries.find(x => x.name == id);
 
         var idx = -1;
 
-        if (match_index == undefined) {
+        // For total time spent
+        if (match_total == undefined) {
             idxs.push({
                 id: id,
                 index: i
@@ -39,9 +58,30 @@ export default function Trends() {
             totalSeries[0].data.push(hours);
             i++;
         } else {
-            idx = match_index.index;
+            idx = match_total.index;
             totalSeries[0].data[idx] += hours;
         }
+
+        // For monthly time spent
+        if (match_month == undefined) {
+            monthlySeries.push({
+                data: new Array(12).fill(0),
+                name: id
+            });
+        } else {
+            match_month.data[month] += hours;
+        }
+
+        // For weekly time spent
+        if (match_week == undefined) {
+            weeklySeries.push({
+                data: new Array(7).fill(0),
+                name: id
+            });
+        } else {
+            match_week.data[weekday] += hours;
+        }
+
     });
 
     console.log(totalSeries);
@@ -71,7 +111,7 @@ export default function Trends() {
                 }
             },
             title: {
-                text: 'Total time logged per category',
+                text: 'Time logged in total per category',
                 style: {
                     fontSize: '16px',
                     fontWeight: 'bold',
@@ -83,29 +123,103 @@ export default function Trends() {
         series: totalSeries
     };
 
+    const stateMonthlyTime = {
+        options: {
+            chart: {
+                id: "basic-bar"
+            },
+            plotOptions: {
+                bar: {
+                    borderRadius: 8
+                }
+            },
+            dataLabels: {
+                enabled: false
+            },
+            xaxis: {
+                categories: monthlyXLabels,
+                title: {
+                    text: "Category"
+                }
+            },
+            yaxis: {
+                title: {
+                    text: "Hours"
+                }
+            },
+            title: {
+                text: 'Time logged per month per category',
+                style: {
+                    fontSize: '16px',
+                    fontWeight: 'bold',
+                    fontFamily: undefined,
+                    color: 'black'
+                }
+            }
+        },
+        series: monthlySeries
+    };
+
+    const stateWeeklyTime = {
+        options: {
+            chart: {
+                id: "basic-bar"
+            },
+            plotOptions: {
+                bar: {
+                    borderRadius: 8
+                }
+            },
+            dataLabels: {
+                enabled: false
+            },
+            xaxis: {
+                categories: weeklyXLabels,
+                title: {
+                    text: "Category"
+                }
+            },
+            yaxis: {
+                title: {
+                    text: "Hours"
+                }
+            },
+            title: {
+                text: 'Time logged per weekday per category',
+                style: {
+                    fontSize: '16px',
+                    fontWeight: 'bold',
+                    fontFamily: undefined,
+                    color: 'black'
+                }
+            }
+        },
+        series: weeklySeries
+    };
+
     return (
         <div>
             <Typography variant={"h1"}>Trends</Typography>
 
             <ReactApexChart
+                type="bar"
                 options={stateTotalTime.options}
                 series={stateTotalTime.series}
-                type="bar"
                 height="350"
                 width="100%"
             />
 
             <ReactApexChart
-                type="line"
-                options={stateTotalTime.options}
-                series={stateTotalTime.series}
+                type="bar"
+                options={stateMonthlyTime.options}
+                series={stateMonthlyTime.series}
                 width="100%"
                 height="350"
             />
             <ReactApexChart
-                type="area"
-                options={stateTotalTime.options}
-                series={stateTotalTime.series}
+                type="bar"
+                options={stateWeeklyTime.options}
+                series={stateWeeklyTime.series}
                 width="100%"
                 height="350"
             />
