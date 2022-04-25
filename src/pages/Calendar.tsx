554c2710db1c320a2acf67d5709a50491b1e2d78
alaym3 <aam5xa@virginia.edu>
2022-04-25 21:04:59 +0200

    Add calendar shell

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -6,34 +6,62 @@ import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import DatePicker from '@mui/lab/DatePicker';
 import {Event, usePersistReducer} from "../service/data";
 import TLItem from "../components/TLItem";
+import FullCalendar, { formatDate } from '@fullcalendar/react' // https://fullcalendar.io/docs/react
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import interactionPlugin from "@fullcalendar/interaction" // needed for dayClick
+import timeGridPlugin from '@fullcalendar/timegrid'
+// https://codesandbox.io/s/github/fullcalendar/fullcalendar-example-projects/tree/master/react?file=/src/DemoApp.jsx
 
 export default function Calendar() {
 
-    const [{categories, categoryNames, events, error}, dispatch] = usePersistReducer() // useReducer(reducer, initialState);
+    const [{categoryNames, events}, dispatch] = usePersistReducer() // useReducer(reducer, initialState);
 
-    // Set the date picker at the top - default to today
-    const [dateValue, setDateValue] = React.useState<Date | null>(new Date());
+    const arrayOfObjects = [
+        { title: "Americano", date: "2022-04-22" },
+        { title: "Espresso", date: "2022-04-23" },
+        { title: "Americano", date: "2022-04-22" },
+        { title: "Espresso", date: "2022-04-23" }
+    ]
+
+    // const [dateValue, setDateValue] = React.useState<Date | null>(new Date());
 
     return (
         <div>
-        <Typography variant={"h2"}>Timeline basic view demo</Typography>
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
-                <DatePicker
-                    label="Select date"
-                    value={dateValue}
-                    onChange={(newValue) => {
-                        setDateValue(newValue);
-                    }}
-                    renderInput={(params) => <TextField {...params} />}
-                />
-            </LocalizationProvider>
+            <Typography color = {"secondary"} variant={"h4"}>Event calendar</Typography>
+            <FullCalendar
+                plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+                headerToolbar={{
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                }}
+                initialView="dayGridMonth"
+                editable={true}
+                selectable={true}
+                selectMirror={true}
+                dayMaxEvents={true}
+                events= {arrayOfObjects}
+            />
 
-            {/*<Typography variant = {"h3"}>try actually accessing the data</Typography>*/}
-            {events?.map((event: Event) => {
-                return <TLItem event={event} eventName={categoryNames[event.categoryId]} />
-            })}
+        {/*<Typography variant={"h2"}>Timeline basic view demo</Typography>*/}
+        {/*    <LocalizationProvider dateAdapter={AdapterDateFns}>*/}
+        {/*        <DatePicker*/}
+        {/*            label="Select date"*/}
+        {/*            value={dateValue}*/}
+        {/*            onChange={(newValue) => {*/}
+        {/*                setDateValue(newValue);*/}
+        {/*            }}*/}
+        {/*            renderInput={(params) => <TextField {...params} />}*/}
+        {/*        />*/}
+        {/*    </LocalizationProvider>*/}
+
+        {/*    /!*<Typography variant = {"h3"}>try actually accessing the data</Typography>*!/*/}
+        {/*    {events?.map((event: Event) => {*/}
+        {/*        return <TLItem event={event} eventName={categoryNames[event.categoryId]} />*/}
+        {/*    })}*/}
 
 
         </div>
 );
 }
+// https://codesandbox.io/s/github/fullcalendar/fullcalendar-example-projects/tree/master/react?file=/src/DemoApp.jsx
