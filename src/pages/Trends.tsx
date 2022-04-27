ee77adc07a4f6dce8b097bf759ba37c4362a3cd2
Thomas Witting <thomaswitting97@gmail.com>
2022-04-27 15:00:54 +0200

    Historical overview plot finalized.

--- src/pages/Trends.tsx
+++ src/pages/Trends.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import {Typography} from "@mui/material";
 import ReactApexChart from "react-apexcharts";
 import {usePersistReducer, Event, Category} from "../service/data";
-import moment from "moment";
+import moment, {Moment} from "moment";
 
 
 // code example for choosing from day, week, month views https://codesandbox.io/s/react-apex-charts-m9tww?file=/src/index.js
@@ -23,8 +23,11 @@ export default function Trends() {
     let weeklyXLabels: string[] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
     let weeklySeries: { name: string, data: number[] }[] = [];
 
+    let timelineSeries: { name: string, data: any[] }[] = [];
+
     let idxs: { name: string, index: number }[] = [];
     let i = 0;
+
     events.forEach((event: Event) => {
 
         let id = event.categoryId;
@@ -34,21 +37,27 @@ export default function Trends() {
 
         let weekday = startTime.isoWeekday() - 1;
         let date = startTime.date(); // 1-based (first day of month = 1)
+
         let month = startTime.month(); // 0-based (jan=0, dec=11)
         let year = startTime.month();
+
+        let millisDate = startTime.clone().startOf('day').valueOf()
 
         var duration = moment.duration(endTime.diff(startTime));
         var hours = duration.asHours();
 
 
-        var match_total = idxs.find(x => x.name == categoryNames[id]);
-        var match_month = monthlySeries.find(x => x.name == categoryNames[id]);
-        var match_week = weeklySeries.find(x => x.name == categoryNames[id]);
+        var match_total = idxs.find(x => x.name === categoryNames[id]);
+        var match_month = monthlySeries.find(x => x.name === categoryNames[id]);
+        var match_week = weeklySeries.find(x => x.name === categoryNames[id]);
+
+        var match_timeline = timelineSeries.find(x => x.name === categoryNames[id]);
+
 
         var idx = -1;
 
         // For total time spent
-        if (match_total == undefined) {
+        if (match_total === undefined) {
             idxs.push({
                 name: categoryNames[id],
                 index: i
@@ -63,7 +72,7 @@ export default function Trends() {
         }
 
         // For monthly time spent
-        if (match_month == undefined) {
+        if (match_month === undefined) {
             monthlySeries.push({
                 data: new Array(12).fill(0),
                 name: categoryNames[id]
@@ -74,7 +83,7 @@ export default function Trends() {
         }
 
         // For weekly time spent
-        if (match_week == undefined) {
+        if (match_week === undefined) {
             weeklySeries.push({
                 data: new Array(7).fill(0),
                 name: categoryNames[id]
@@ -84,6 +93,31 @@ export default function Trends() {
             match_week.data[weekday] += hours;
         }
 
+        // For timeline view
+        if (match_timeline === undefined) {
+            timelineSeries.push({
+                data: [[millisDate, hours]],
+                name: categoryNames[id]
+            });
+        } else {
+            let match_date = match_timeline.data.find(x => x[0] === millisDate);
+            if (match_date === undefined) {
+                match_timeline.data.push([millisDate, hours]);
+            } else {
+                match_date[1] += hours;
+            }
+        }
+
+        /*
+        // For timeline view
+        if (match_timeline === undefined) {
+            timelineSeries.push({
+                data: [moment(event.startTime).clone().startOf('day').toDate()],
+                name: categoryNames[id]
+            });
+        } else {
+            match_timeline.data.push(moment(event.startTime).clone().startOf('day').toDate());
+        }*/
     });
 
     console.log(totalSeries);
@@ -214,9 +248,64 @@ export default function Trends() {
         series: weeklySeries
     };
 
+    const stateTimeline = {
+        options: {
+            chart: {
+                id: "basic-bar"
+            },
+            plotOptions: {
+                bar: {
+                    borderRadius: 2
+                }
+            },
+            dataLabels: {
+                enabled: false
+            },
+            xaxis: {
+                title: {
+                    text: "Date"
+                },
+                labels: {
+                    formatter: function (millis: string) {
+                        return moment(millis).format("DD MMM YYYY");
+                    }
+                }
+            },
+            yaxis: {
+                title: {
+                    text: "Hours"
+                },
+                labels: {
+                    formatter: function (num: number) {
+                        return num.toFixed(3)
+                    }
+                }
+            },
+            title: {
+                text: 'Historical overview',
+                style: {
+                    fontSize: '16px',
+                    fontWeight: 'bold',
+                    fontFamily: undefined,
+                    color: 'black'
+                }
+            }
+        },
+        series: timelineSeries
+    };
+
     return (
         <div>
             <Typography color={"secondary"} variant={"h4"}>Trends</Typography>
+
+
+            <ReactApexChart
+                type="line"
+                options={stateTimeline.options}
+                series={stateTimeline.series}
+                width="100%"
+                height="350"
+            />
 
             <ReactApexChart
                 type="bar"
