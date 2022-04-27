f55c8f7024a048588bc0c0bd07357c949109348b
alaym3 <aam5xa@virginia.edu>
2022-04-27 11:34:58 +0200

    Adjust calendar height

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import {Typography} from "@mui/material";
 import {Event, usePersistReducer} from "../service/data";
-import FullCalendar from '@fullcalendar/react' // https://fullcalendar.io/docs/react
+import FullCalendar, {DateInput, formatDate} from '@fullcalendar/react' // https://fullcalendar.io/docs/react
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 import interactionPlugin from "@fullcalendar/interaction" // needed for dayClick
 import timeGridPlugin from '@fullcalendar/timegrid'
 // https://codesandbox.io/s/github/fullcalendar/fullcalendar-example-projects/tree/master/react?file=/src/DemoApp.jsx
+
 
 export default function Calendar() {
 
@@ -19,24 +20,68 @@ export default function Calendar() {
         })
     })
 
-    return (
-        <div>
-            <Typography color={"secondary"} variant={"h4"}>Event calendar</Typography>
-            <FullCalendar
-                plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
-                headerToolbar={{
-                    left: 'prev,next today',
-                    center: 'title',
-                    right: 'dayGridMonth,timeGridWeek,timeGridDay'
-                }}
-                initialView="dayGridMonth"
-                editable={true}
-                selectable={true}
-                selectMirror={true}
-                dayMaxEvents={true}
-                events={arrayOfObjects}
-            />
-        </div>
-    );
+    // allow deleting events from the calendar
+    // (should we only do this on a long click? what if people want to see more info on what they did..??
+    let handleEventClick = (clickInfo: { event: { title: any; remove: () => void; }; }) => {
+        // eslint-disable-next-line no-restricted-globals
+        if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'?`)) {
+            clickInfo.event.remove()
+            // UPDATE THIS TO HAVE THE RIGHT EVENT ID AND CALL DISPATCH
+        }
+    }
+
+    function createEventId() {
+
+    }
+
+    // allow adding events on the calendar
+    let handleDateSelect = (selectInfo: { view: { calendar: any; }; startStr: any; endStr: any; allDay: any; }) => {
+        let title = prompt('Please enter a new title for your event')
+        let calendarApi = selectInfo.view.calendar
+
+        calendarApi.unselect() // clear date selection
+
+        if (title) {
+            calendarApi.addEvent({
+                id: createEventId(),
+                title,
+                start: selectInfo.startStr,
+                end: selectInfo.endStr,
+                allDay: selectInfo.allDay
+            })
+        }
+    }
+
+
+        return (
+            <div>
+                <Typography color={"secondary"} variant={"h4"}>Event calendar</Typography>
+                <FullCalendar
+                    plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+                    headerToolbar={{
+                        left: 'prev,next today',
+                        center: 'title',
+                        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                    }}
+                    initialView="dayGridMonth"
+                    editable={true}
+                    selectable={true}
+                    selectMirror={true}
+                    dayMaxEvents={true}
+                    //contentHeight={"auto"}
+                    stickyHeaderDates={true}
+                    height={"auto"} // ???? what to do?
+                    events={arrayOfObjects}
+                    //testing deleting/adding events
+                    eventClick={handleEventClick} // deleting event
+                    select={handleDateSelect} // adding event
+                />
+
+
+
+            </div>
+        );
+
+
 }
 // https://codesandbox.io/s/github/fullcalendar/fullcalendar-example-projects/tree/master/react?file=/src/DemoApp.jsx
